78067f2ac675a744c17808ca962c09cf8e5ce860
CristopherGuerrero <cristopherguerrero325@gmail.com>
2025-02-08 22:06:23 -0400

    Add files via upload

---
+++ Tarea 2 Par o Impar/Tarea 2 Par o Impar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class ParImpar
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine(numero + " es par.");
+        }
+        else
+        {
+            Console.WriteLine(numero + " es impar.");
+        }
+    }
+}
