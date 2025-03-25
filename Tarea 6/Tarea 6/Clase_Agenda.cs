0f1abde6b177f546a5601f47c77fbc4e5b8950aa
CristopherGuerrero <cristopherguerrero325@gmail.com>
2025-03-25 11:37:23 -0400

    Add files via upload

--- Tarea 6/Tarea 6/Clase_Agenda.cs
+++ Tarea 6/Tarea 6/Clase_Agenda.cs
@@ -5,14 +5,12 @@ public class Agenda
 {
     private List<Contacto> contactos = new List<Contacto>();
 
-    // Método para agregar un contacto
     public void AgregarContacto(Contacto contacto)
     {
         contactos.Add(contacto);
         Console.WriteLine($"Contacto {contacto.Nombre} agregado exitosamente.");
     }
 
-    // Método para ver todos los contactos
     public void VerContactos()
     {
         Console.WriteLine("Lista de Contactos:");
@@ -22,13 +20,11 @@ public class Agenda
         }
     }
 
-    // Método para buscar un contacto por Id
     public Contacto BuscarContacto(int id)
     {
         return contactos.Find(c => c.Id == id);
     }
 
-    // Método para modificar un contacto
     public void ModificarContacto(int id, string nombre, string telefono, string email, string direccion)
     {
         Contacto contacto = BuscarContacto(id);
@@ -46,7 +42,6 @@ public class Agenda
         }
     }
 
-    // Método para eliminar un contacto
     public void EliminarContacto(int id)
     {
         Contacto contacto = BuscarContacto(id);
@@ -62,7 +57,7 @@ public class Agenda
     }
     public List<Contacto> ObtenerContactos()
     {
-        return contactos; // Retorna la lista completa de contactos
+        return contactos; 
     }
 }
 
